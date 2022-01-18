79deea3c946528cb0d33d5d94503e859dd37f2fd
Paul Maskelyne <mu77ley@gmail.com>
2022-01-18 20:18:03 +0000

    rearrange gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,49 @@
-const gulp = require("gulp");
+const eslint      = require("gulp-eslint7");
+const gulp        = require("gulp");
+const gulpIf      = require("gulp-if");
+const mergeStream = require("merge-stream");
+const yaml        = require('gulp-yaml');
 
-// const css = require("./utils/css.js");
-const linting = require("./utils/lint.js");
-const compileLangs = require("./utils/langs.js");
-// const packs = require("./utils/packs.js");
+const parsedArgs  = require("yargs").argv;
 
+const LINTER_PATHS    = ["./edrpg/edrpg.js", "./edrpg/module/"];
+const YAML_LANG_PATHS = ["yaml/i18n/*.yaml"];
 
-// exports.default = gulp.series(
-// gulp.parallel(css.compile),
-// css.watchUpdates
-// );
-// exports.css = css.compile;
-// exports.cleanPacks = gulp.series(packs.clean);
-// exports.compilePacks = gulp.series(packs.compile);
-// exports.extractPacks = gulp.series(packs.extract);
-exports.lint = gulp.series(linting.lint);
-exports.langs = gulp.series(compileLangs.langs)
-// exports.buildAll = gulp.parallel(
-//   css.compile,
-//   packs.compile
-// );
+function linterTask() {
+	const applyFixes = !!parsedArgs.fix;
+
+	const tasks = LINTER_PATHS.map(path => {
+		const src = path.endsWith("/")
+			? `${path}**/*.js`
+			: path;
+
+		const dest = path.endsWith("/")
+			? path
+			: `${path.split("/").slice(0, -1).join("/")}/`;
+
+		return gulp
+			.src(src)
+			.pipe(eslint({fix: applyFixes}))
+			.pipe(eslint.format())
+			.pipe(gulpIf(file => file.eslint != null && file.eslint.fixed, gulp.dest(dest)));
+	});
+
+	return mergeStream.call(null, tasks);
+}
+
+function yamlLangTask() {
+  return gulp.src(YAML_LANG_PATHS)
+    .pipe(yaml({ space: 2 }))
+    .pipe(gulp.dest("./edrpg/i18n"))
+}
+
+const defaultTask = gulp.series(
+	linterTask,
+	yamlLangTask,
+
+);
+
+exports.default = defaultTask;
+
+exports.lint = gulp.series(linterTask);
+exports.langs = gulp.series(yamlLangTask)
