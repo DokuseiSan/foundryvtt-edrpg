a48f7d1e55b17877a2dadd4383a3deeed80ee320
Paul Maskelyne <mu77ley@gmail.com>
2022-01-19 18:02:46 +0000

    remove unecessary node packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
 "use strict";
 
-const eslint      = require("gulp-eslint-new");
-const gulp        = require("gulp");
-const gulpIf      = require("gulp-if");
-const mergeStream = require("merge-stream");
-const yaml        = require('gulp-yaml');
-const sass = require('gulp-sass')(require('sass'));
+const eslint = require("gulp-eslint-new");
+const gulp   = require("gulp");
+const gulpIf = require("gulp-if");
+const sass   = require("gulp-sass")(require("sass"));
+const yaml   = require("gulp-yaml");
 
+const mergeStream = require("merge-stream");
 const parsedArgs  = require("yargs").argv;
 
-const LINTER_PATHS    = ["./edrpg/edrpg.js", "./edrpg/module/"];
-const YAML_LANG_PATHS = ["yaml/i18n/*.yaml"];
+const LINT_PATHS = ["./edrpg/edrpg.js", "./edrpg/module/"];
+const LANG_PATHS = ["yaml/i18n/*.yaml"];
 
-function buildStyles() {
-	return gulp.src("./scss/**/*.scss")
-		.pipe(sass.sync().on("error", sass.logError))
-		.pipe(gulp.dest("./edrpg/css"));
+// transform all the i18n language YAML files into JSON within the main system
+// directory
+//
+function buildLangs(cb) {
+	gulp.src(LANG_PATHS)
+		.pipe(yaml({ space: 2 }))
+		.pipe(gulp.dest("./edrpg/i18n"));
+
+	cb();
 }
 
-function lintJavascript() {
+function buildStyles(cb) {
+	gulp.src("./scss/**/*.scss")
+		.pipe(sass.sync().on("error", sass.logError))
+		.pipe(gulp.dest("./edrpg/css"));
+
+	cb();
+}
+
+function lintJavascript(cb) {
 	const applyFixes = !!parsedArgs.fix;
 
-	const tasks = LINTER_PATHS.map(path => {
+	const tasks = LINT_PATHS.map(path => {
 		const src = path.endsWith("/")
 			? `${path}**/*.js`
 			: path;
@@ -32,18 +45,14 @@ function lintJavascript() {
 
 		return gulp
 			.src(src)
-			.pipe(eslint({fix: applyFixes}))
+			.pipe(eslint({ fix: applyFixes }))
 			.pipe(eslint.format())
 			.pipe(gulpIf(file => file.eslint != null && file.eslint.fixed, gulp.dest(dest)));
 	});
 
-	return mergeStream.call(null, tasks);
-}
+	mergeStream(tasks);
 
-function buildLangs() {
-  return gulp.src(YAML_LANG_PATHS)
-    .pipe(yaml({ space: 2 }))
-    .pipe(gulp.dest("./edrpg/i18n"))
+	cb();
 }
 
 const defaultTask = gulp.parallel(
@@ -54,10 +63,10 @@ const defaultTask = gulp.parallel(
 
 exports.default = defaultTask;
 
+exports.buildLangs     = buildLangs;
+exports.buildStyles    = buildStyles;
 exports.lintJavascript = lintJavascript;
-exports.buildLangs = buildLangs;
-exports.buildStyles = buildStyles;
 
-exports.watch = function () {
-  gulp.watch('./scss/**/*.scss', buildStyles);
+exports.watch = function() {
+	gulp.watch("./scss/**/*.scss", buildStyles);
 };
